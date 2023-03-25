e970fcbb6d7eeaf7d7dcef48993fff8b30c4b908
matty57420 <maruliermatthieu@gmail.com>
2023-03-25 17:46:34 +0100

    Initial commit

---
+++ src/test/java/com/mattydev/bankmanagement/bankmanagement/BankManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mattydev.bankmanagement.bankmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
